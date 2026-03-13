9321fbcd4f64e2f9db7c4769e11c763530fa91fe
Chandan Pathak <vijaypathakssa3@gmail.com>
2026-03-13 21:29:18 +0530

    chore: Remove various data migration, verification, and utility scripts and temporary files.

--- generate_token.js
+++
@@ -1,4 +0,0 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const token = jwt.sign({ userId: 1 }, process.env.JWT_ACCESS_SECRET || 'supersecretaccess123', { expiresIn: '1h' });
-console.log(token);
